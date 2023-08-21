c96a25bb37d1b19dccf4e80ff5f026b72f32fc30
wallchristopher <wall.christopher.d@gmail.com>
2023-08-21 13:33:23 -0500

    moved away from v1beta1

--- infra/providers.tf
+++ infra/providers.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
 provider "kubernetes" {
@@ -7,7 +7,7 @@ provider "kubernetes" {
   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
 
   exec {
-    api_version = "client.authentication.k8s.io/v1beta1"
+    api_version = "client.authentication.k8s.io/v1"
     command     = "aws"
     # This requires the awscli to be installed locally where Terraform is executed
     args = ["eks", "get-token", "--cluster-name", module.eks.cluster_name]
