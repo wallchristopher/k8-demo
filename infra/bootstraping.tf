2360c15571ad02f580ca540c844ef385d93bd569
Christopher Wall <wall.christopher.d@gmail.com>
2024-02-06 20:07:53 -0600

    updated deployment of helm and kubeconfig

--- infra/bootstraping.tf
+++ infra/bootstraping.tf
@@ -1,21 +1,33 @@
-data "utils_aws_eks_update_kubeconfig" "k8s_platform" {
-  alias        = local.name
-  cluster_name = local.name
-  profile      = "default"
+resource "null_resource" "kubectl" {
+  provisioner "local-exec" {
+    command = "aws eks update-kubeconfig --name k8s-platform --kubeconfig ~/.kube/eks"
+  }
 
   depends_on = [module.fargate_eks]
 }
 
-resource "helm_release" "argocd" {
+resource "helm_release" "argo_bootstrap" {
   name              = "argocd"
-  chart             = "${path.module}/../bootstrap/argo/"
+  chart             = "argo-cd"
   namespace         = "argocd"
+  repository        = "https://argoproj.github.io/argo-helm"
   create_namespace  = true
   dependency_update = true
+
+  depends_on = [null_resource.kubectl]
+}
+
+resource "helm_release" "argocd" {
+  name      = "argocd"
+  chart     = "${path.module}/../bootstrap/argo/"
+  namespace = "argocd"
 
   values = [
     "${file("${path.module}/../bootstrap/argo/values-${var.environment}.yaml")}"
   ]
 
-  depends_on = [data.utils_aws_eks_update_kubeconfig.k8s_platform]
+  depends_on = [
+    null_resource.kubectl,
+    helm_release.argo_bootstrap
+  ]
 }
